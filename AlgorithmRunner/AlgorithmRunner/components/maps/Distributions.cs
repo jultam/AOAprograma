add2646ceb59e3f2afc9e65435ee911fba44898b
julius-tamasas-bentley <julius.tamasas@bentley.com>
2026-04-01 14:58:13 +0300

    rename and librarby

--- AlgorithmRunner/AlgorithmRunner/components/maps/Distributions.cs
+++ AlgorithmRunner/AlgorithmRunner/components/maps/Distributions.cs
@@ -1,4 +1,5 @@
 ﻿using Meta.Numerics.Functions;
+using MathNet.Numerics.Distributions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,17 +17,36 @@ namespace CONTOPT
         private static Random _rnd = new Random();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double BetaDistribution()
+        public static double BetaDist()
         {
             double a = 3; double b = 2; double x = _rnd.NextDouble();
-            double gammas = AdvancedMath.Gamma(a + b) / (AdvancedMath.Gamma(a) + AdvancedMath.Gamma(b));
-            double X = gammas * Math.Pow(x, a - 1) * Math.Pow(1 - x, b - 1);
-            //double normX = x
+            //double gammas = AdvancedMath.Gamma(a + b) / (AdvancedMath.Gamma(a) + AdvancedMath.Gamma(b));
+            //double X = gammas * Math.Pow(x, a - 1) * Math.Pow(1 - x, b - 1);
+            lastX = Beta.Sample(a, b);
             return lastX;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double UniformDistribution()
+        public static double RayleighDist()
+        {
+            lastX = Rayleigh.Sample(0.4);
+            return Rayleigh.Sample(0.4);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double LatinHypercubeSample()
+        {
+            return Rayleigh.Sample(0.4);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double SobolLowDiscrepancySeq()
+        {
+            return Rayleigh.Sample(0.4);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double UniformDist()
         {
             return _rnd.NextDouble();
         }
