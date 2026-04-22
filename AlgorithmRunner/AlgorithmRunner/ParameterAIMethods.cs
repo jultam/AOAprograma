59b28671ff02d88ee10cd033aa81c59e07347298
Julius <60598199+jultam@users.noreply.github.com>
2026-04-23 00:03:18 +0300

    torchsharp start

---
+++ AlgorithmRunner/AlgorithmRunner/ParameterAIMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TorchSharp;
+using static TorchSharp.torch.nn;
+
+namespace AlgorithmRunner
+{
+    internal class ParameterAIMethods
+    {
+        private static ParameterModel model;
+
+        public static void CreateModel()
+        {
+            model = new ParameterModel("parameterModel");
+        }
+
+        public static void LoadModel(string filePath)
+        {
+            model.load(filePath);
+        }
+
+        public static void TrainModel(int iter)
+        {
+            var optimizer = torch.optim.Adam(model.parameters(), lr: 0.01);
+
+            var input = torch.randn(64, 5);
+            var target = torch.randn(64, 5);
+
+            for (int i = 0; i < iter; i++)
+            {
+                var eval = model.forward(input);
+                var output = functional.mse_loss(eval, target, Reduction.Sum);
+
+                optimizer.zero_grad();
+
+                output.backward();
+
+                optimizer.step();
+            }
+        }
+
+        public static void SaveModel(string filePath)
+        {
+            model.save(filePath);
+        }
+
+    }
+}
---
+++ AlgorithmRunner/AlgorithmRunner/ParameterModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TorchSharp;
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+namespace AlgorithmRunner
+{
+    public class ParameterModel : Module<Tensor, Tensor>
+    {
+        private readonly Module<Tensor, Tensor> layers;
+
+        public ParameterModel(string name) : base(name) 
+        {
+            layers = Sequential(
+                Linear(5, 7),
+                ReLU(), 
+                Dropout(0.1), 
+                Linear(7, 5)
+            );
+            RegisterComponents();
+        }
+
+        public override Tensor forward(Tensor input)
+        {
+            return layers.forward(input);
+        }
+    }
+}
